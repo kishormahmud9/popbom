f01e9569b3a43c98a9c87478f51ff74b7d14adf0
Munna <mehedihasanmunna516@gmail.com>
2025-12-20 14:02:20 +0600

    update cors

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app:Application = express()
 
 // parsers
 app.use(express.json());
-app.use(cors({origin: ['http://localhost:5173'], credentials:true}))
+app.use(cors({origin: ['http://localhost:5173',"http://172.252.13.97:5000"], credentials:true}))
 
 // application routes
 app.use('/api', router);
