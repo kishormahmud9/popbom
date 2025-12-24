d0636403811dcff1edbf0250b42b6233fc24eda8
kishormahmud9 <kishor.betopia@gmail.com>
2025-12-24 15:51:39 +0600

    add admin info

--- src/module/AdminAuth/adminAuth.service.ts
+++ src/module/AdminAuth/adminAuth.service.ts
@@ -1,41 +1,49 @@
-import httpStatus from "http-status";
-import { Admin } from "./adminAuth.modal";
-import { TLoginAdmin } from "./adminAuth.interface";
-import AppError from "../../app/errors/AppError";
-import { createToken } from "./adminAuth.utils";
-import config from "../../app/config";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import httpStatus from 'http-status';
+import { Admin } from './adminAuth.modal';
+import { TLoginAdmin } from './adminAuth.interface';
+import AppError from '../../app/errors/AppError';
+import { createToken } from './adminAuth.utils';
+import config from '../../app/config';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 /* 🔹 Login Admin */
 const login = async (payload: TLoginAdmin) => {
-    const admin = await Admin.isAdminExistByEmail(payload.email);
-    if (!admin) {
-        throw new AppError(httpStatus.NOT_FOUND, "Admin not found");
-    }
+  const admin = await Admin.isAdminExistByEmail(payload.email);
+  if (!admin) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Admin not found');
+  }
 
-    const isPasswordMatched = await Admin.isPasswordMatched(
-        payload.password,
-        admin.password
-    );
+  const isPasswordMatched = await Admin.isPasswordMatched(
+    payload.password,
+    admin.password,
+  );
 
-    if (!isPasswordMatched) {
-        throw new AppError(httpStatus.UNAUTHORIZED, "Password is incorrect");
-    }
-    const jwtPayload = {
-        adminId: admin._id!.toString(),
-        email: admin.email,
-        role: admin.role || 'admin',
-    };
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.UNAUTHORIZED, 'Password is incorrect');
+  }
+  const jwtPayload = {
+    adminId: admin._id!.toString(),
+    email: admin.email,
+    role: admin.role || 'admin',
+  };
 
-    const accessToken = createToken(jwtPayload, config.jwt_access_secret as string);
-    const refreshToken = createToken(jwtPayload, config.jwt_refresh_secret as string);
+  const accessToken = createToken(
+    jwtPayload,
+    config.jwt_access_secret as string,
+  );
+  const refreshToken = createToken(
+    jwtPayload,
+    config.jwt_refresh_secret as string,
+  );
 
-    return {
-        accessToken,
-        refreshToken
-    };
+  return {
+    accessToken,
+    refreshToken,
+    adminId: admin._id!.toString(),
+    email: admin.email,
+  };
 };
 
 export const AdminAuthService = {
-    login,
+  login,
 };
