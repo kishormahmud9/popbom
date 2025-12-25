316dc7439ef02a1f6bd8be151fc9e246991471e7
Munna <mehedihasanmunna516@gmail.com>
2025-12-25 17:20:59 +0600

    update file

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,20 @@ import cors from 'cors';
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import notFound from "./app/middleware/notFound";
 import router from "./app/routes";
-import cookieParser from "cookie-parser";
+// import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser';
 
-const app:Application = express()
+const app: Application = express()
 
 // parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: ['http://localhost:3001',"http://localhost:3000","http://localhost:3003"], credentials:true}))
+app.use(cors({ origin: ['http://localhost:3001', "http://localhost:3000", "http://localhost:3003"], credentials: true }))
 
 // application routes
 app.use('/api', router);
 
-app.get('/', (req:Request, res:Response) =>{
+app.get('/', (req: Request, res: Response) => {
     res.send('Pop Bom is running')
 })
 
